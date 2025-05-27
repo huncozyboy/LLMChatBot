3102ab74a505696407de4b7fd87a7b2872e3b486
huncozyboy <leejh5838@naver.com>
2025-05-27 16:34:44 +0900

    refactor : Few Shot을 활용한 답변 정확도 향상과 포맷 수정

--- llm.py
+++ llm.py
@@ -1,6 +1,6 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import MessagesPlaceholder, FewShotChatMessagePromptTemplate
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
@@ -10,6 +10,8 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from config import answer_examples
 
 store = {}
 
@@ -26,6 +28,32 @@ def get_retriever():
     database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
     retriever = database.as_retriever(search_kwargs={'k': 4})
     return retriever
+
+
+def get_history_retriver():
+    llm = get_llm()
+    retriever = get_retriever()
+
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    return history_aware_retriever 
 
 
 def get_llm(model='gpt-4o'):
@@ -51,43 +79,34 @@ def get_dicionary_chain():
 
 def get_rag_chain():
     llm = get_llm()
-    retriever = get_retriever()
-
-    contextualize_q_system_prompt = (
-        "Given a chat history and the latest user question "
-        "which might reference context in the chat history, "
-        "formulate a standalone question which can be understood "
-        "without the chat history. Do NOT answer the question, "
-        "just reformulate it if needed and otherwise return it as is."
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{answer}"),
+        ]
     )
-
-    contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-    )
-
-    history_aware_retriever = create_history_aware_retriever(
-        llm, retriever, contextualize_q_prompt
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=answer_examples,
     )
     system_prompt = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
+        " 당신은 소득세법 전문가입니다. 사용자의 소득세법과 관한 질문에 답변해주세요"
+        " 아래 제공된 검색된 문서를 활용해서 질문에 답변 해주세요"
+        " 만약 답을 모른다면, 모른다고 솔직히 답변 해주세요 "
+        " 답변할땐 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고, "
+        " 답변은 최대 세 문장으로 간결하게 답변해주세요 "
         "\n\n"
         "{context}"
     )
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
+            few_shot_prompt,
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),
         ]
     )
+    history_aware_retriever = get_history_retriver()
     question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
     rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
