2cdcd5c6ee9decccb15ea8982a6d018d97bab2fd
huncozyboy <leejh5838@naver.com>
2025-05-27 15:45:59 +0900

    refactor : retriever, llm, dictionarychain 별도 메서드로 분리

--- llm.py
+++ llm.py
@@ -6,22 +6,22 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
-def get_ai_message(user_message):
+def get_retriever():
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     index_name = 'tex-index'
     database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull("rlm/rag-prompt")
     retriever = database.as_retriever(search_kwargs={'k': 4})
+    return retriever
 
-    qa_chain = RetrievalQA.from_chain_type(
-        llm, 
-        retriever=retriever,
-        chain_type_kwargs={"prompt": prompt}
-    )
 
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model='gpt-4o')
+    return llm
+
+
+def get_dicionary_chain():
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
-
+    llm = get_llm()
     prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
         만약 변경할 필요가 없다고 판단된다면, 사용자의 질문을 변경하지 않아도 됩니다.
@@ -32,6 +32,23 @@ def get_ai_message(user_message):
     """)
 
     dictionary_chain = prompt | llm | StrOutputParser()
+    return dictionary_chain
+
+
+def get_qa_chain():
+    llm = get_llm()
+    retriever = get_retriever()
+    prompt = hub.pull("rlm/rag-prompt")
+    qa_chain = RetrievalQA.from_chain_type(
+        llm, 
+        retriever=retriever,
+        chain_type_kwargs={"prompt": prompt}
+    )
+    return qa_chain
+
+def get_ai_message(user_message):
+    dictionary_chain = get_dicionary_chain()
+    qa_chain = get_qa_chain()
     tax_chain = {"query": dictionary_chain} | qa_chain
     ai_message = tax_chain.invoke({"question": user_message})
-    return ai_message
+    return ai_message['result']
